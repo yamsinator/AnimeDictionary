728574e481b75ee54df49abdefe75e45f1fbcd17
yamsinator <BuiSteven@gmail.com>
2024-04-06 02:38:43 -0700

    Created 3 new methods for anime, manga, and character

--- src/test/java/testsrc/APITEST.java
+++ src/test/java/testsrc/APITEST.java
@@ -1,124 +1,77 @@
 package testsrc;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class AnimeDictionaryTest {
+public class APITEST {
 
-	public static JSONObject getAnimeData(String animeName) {
+	private static final String API_BASE_URL = "https://api.jikan.moe/v4/";
 
-		JSONArray animeID = getAnimeID(animeName);
-
-		// Extract the ID data
-		JSONObject id = (JSONObject) animeID.get(0);
-		int animeCode = (int) id.get("id");
-
-		// Build API request URL with the requested ID
-		String urlString = "https://api.myanimelist.net/v2/anime/" + animeCode;
-
-		try {
-			// Call API and get response
-			HttpURLConnection conn = fetchApiResponse(urlString);
-
-			// check for response status
-			// 200 - means that the connection was a success
-			if (conn.getResponseCode() != 200) {
-				System.out.println("Error: Could not connect to API");
-				return null;
-			}
-			// Store resulting JSON data
-			StringBuilder resultJSON = new StringBuilder();
-			Scanner scanner = new Scanner(conn.getInputStream());
-			while (scanner.hasNext()) {
-				// read and store into the string builder
-				resultJSON.append(scanner.nextLine());
-			}
-
-			// close scanner
-			scanner.close();
-
-			// close url connection
-			conn.disconnect();
-			
-			// Parse through the data to get info on anime search
-			JSONParser parser = new JSONParser();
-			JSONObject resultJSONObj = (JSONObject)parser.parse(String.valueOf(resultJSON));
-			
-			// Retrieve title
-			JSONObject title = (JSONObject)resultJSONObj.get("title");
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return animeName;
+	public List<JSONObject> searchAnimeByName(String animeName) {
+		return performSearch("anime", animeName);
 	}
 
-	private static JSONArray getAnimeID(String animeName) {
-
-		// Build API url with the anime name
-		String urlString = "https://api.myanimelist.net/v2/" + animeName;
-
-		try {
-
-			// Call on the API and get a response
-			HttpURLConnection conn = fetchApiResponse(urlString);
-
-			// 200 means successful conneciton
-			if (conn.getResponseCode() != 200) {
-				System.out.println("Error: Could not connect to API");
-				return null;
-			} else {
-				// Store the API results
-				StringBuilder resultJSON = new StringBuilder();
-				Scanner scanner = new Scanner(conn.getInputStream());
-
-				// Read and store the resulting JSON data into the StringBuilder
-				while (scanner.hasNext()) {
-					resultJSON.append(scanner.nextLine());
-				}
-
-				// Close scanner
-				scanner.close();
-				// Close URL connection
-				conn.disconnect();
-
-				// Parse the JSON string into a JSON Object
-				JSONParser parser = new JSONParser();
-				JSONObject resultJSONObj = (JSONObject) parser.parse(String.valueOf(resultJSON));
-
-				// Get the anime ID from the API
-				JSONArray animeIdData = (JSONArray) resultJSONObj.get("id");
-				return animeIdData;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return null;
+	public List<JSONObject> searchMangaByName(String mangaName) {
+		return performSearch("manga", mangaName);
 	}
 
-	private static HttpURLConnection fetchApiResponse(String urlString) {
+	public List<JSONObject> searchCharactersByName(String charName) {
+		return performSearch("characters", charName);
+	}
+
+	public List<JSONObject> performSearch(String type, String searchQuery) {
+
+		List<JSONObject> resultsList = new ArrayList<>();
+
 		try {
-			// attempt to create connection
+			// Construct URL for anime search
+			String urlString = API_BASE_URL + type + "?q=" + searchQuery.replace(" ", "%20");
 			URL url = new URL(urlString);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-			// set request method to get
+			// Open connection
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 
-			// connect to our API
-			conn.connect();
-			return conn;
-		} catch (Exception e) {
+			// Check response code
+			int responseCode = conn.getResponseCode();
+			if (responseCode != 200) {
+				throw new IOException("Unexpected HTTP response code: " + responseCode);
+			}
+
+			// Read response into a JSON object
+			Scanner scanner = new Scanner(url.openStream());
+			StringBuilder response = new StringBuilder();
+			while (scanner.hasNext()) {
+				response.append(scanner.nextLine());
+			}
+			scanner.close();
+
+			// Parse JSON response
+			JSONObject jsonResponse = new JSONObject(response.toString());
+
+			// Get results array
+			JSONArray results = jsonResponse.getJSONArray("data");
+			for (int i = 0; i < results.length(); i++) {
+				JSONObject animeInfo = results.getJSONObject(i);
+
+				// Retrieve image URL and add to animeInfo
+				if (animeInfo.has("small_image_url")) {
+					String imageUrl = animeInfo.getString("small_image_url");
+					animeInfo.put("small_image_url", imageUrl); // Update small_image_url field
+				}
+
+				resultsList.add(animeInfo);
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return resultsList;
 	}
-
 }
